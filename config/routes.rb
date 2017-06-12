7bac9fef51aed78a966b0148b392f8a9ee792cd7
Stefano Cattoni <stefanocattoni98@gmail.com>
2017-06-12 16:45:35 +0200

    Añadir controlador static

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'statics/index'
+
+  get 'statics/about'
+
   get 'peliculas/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
