a535ca1ca81d31afaab6cf9b666c686efc22b49f
Stefano Cattoni <stefanocattoni98@gmail.com>
2017-06-12 19:05:57 +0200

    Controlador peliculas

--- app/controllers/peliculas_controller.rb
+++ app/controllers/peliculas_controller.rb
@@ -1,4 +1,60 @@
 class PeliculasController < ApplicationController
-  def index
-  end
+before_action :authenticate_user!, except: [:index, :show]
+   def index
+      @peliculas = Pelicula.all
+      #muestra las peliculas que hay en la base de datos
+   end
+   
+   def show
+      @pelicula = Pelicula.find(params[:id])
+      #muestra una pelicula
+   end
+   
+   def new
+      @pelicula = Pelicula.new
+      #crea una pelicula
+   end
+   
+   def create
+      @pelicula = Pelicula.new(pelicula_params)
+      #no tiene vista, procesa la pelicula
+      if @pelicula.save
+         redirect_to root_path, :notice => "Se ha guardado la peli"
+      else
+         render "new"
+      end
+   end
+   
+   def edit
+      @pelicula = Pelicula.find(params[:id])
+      #actualiza la peli
+   end
+   
+ 
+   def update
+      @pelicula = Pelicula.find(params[:id])
+    
+      if @pelicula.update_attributes(pelicula_params)
+         redirect_to root_path, :notice => "éxito!"
+      else
+       render "edit"
+      end
+   end
+
+   
+   def destroy
+      @pelicula = Pelicula.find(params[:id])
+      @pelicula.destroy
+      redirect_to root_path, :notice => "Eliminado"
+
+      #render "index"
+   end
+
+   private
+
+      def pelicula_params
+            params.require(:pelicula).permit(:name, :main_actor, :stars, :year )
+      
+      end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,6 @@ Rails.application.routes.draw do
 
   get 'peliculas/index'
 
+  resources :peliculas
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
