784dfb809e74b6831dbad7b34d5278ef8b2cbb47
Stefano Cattoni <stefanocattoni98@gmail.com>
2017-06-14 18:49:26 +0200

    No major changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_one :setting
+  after_destroy :same_callback_method
+
+    def same_callback_method
+    	destroy :setting, :user_id;
+  	end
 end
