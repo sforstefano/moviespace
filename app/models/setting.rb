bf8f99a2490fa573995291e3714853657633fc60
Stefano Cattoni <stefanocattoni98@gmail.com>
2017-06-14 19:14:49 +0200

    More minnor changes

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,3 +1,5 @@
 class Setting < ApplicationRecord
   belongs_to :user
+  validates :country, :language, presence: :true
+  validates :country, :language, length: { minimum: 2, message: "Mínimo 2 caracteres" }
 end
