3b6d1f24bda30c72ccafc5e35d079a004a95d7e5
Stefano Cattoni <stefanocattoni98@gmail.com>
2017-06-18 18:29:59 +0200

    Dropdown menu

--- app/controllers/statics_controller.rb
+++ app/controllers/statics_controller.rb
@@ -3,10 +3,10 @@ class StaticsController < ApplicationController
   def index
   	  if params[:search]
          @peliculas = Pelicula.search(params[:search])
-      elsif 
+      elsif params[:search_year]
       	 @peliculas = Pelicula.search_year(params[:search_year])
-      elsif
-      	 @peliculas = Pelicula.search_genre(params[:search_genre])
+      elsif params[:search_genre]
+      	 @peliculas = Pelicula.search_genre(params[:search_genre]).order("created_at DESC")
       else
       	 @peliculas = Pelicula.all
       end
--- app/models/pelicula.rb
+++ app/models/pelicula.rb
@@ -5,11 +5,11 @@ class Pelicula < ApplicationRecord
      	where("main_actor like ?", "%" + "%#{search}%" + "%")
    	end
 
-   	def self.search_year(search_year)
-   		where(year: search_year)
-   	end
+  def self.search_year(search_year)
+   where(year: search_year)
+  end
 
-   	def self.search_genre(search_genre)
-   		where(genre: search_genre)
-   	end
+  def self.search_genre(search_genre)
+    where("genre like ?", "%" + "%#{search_genre}%" + "%")
+  end
 end
