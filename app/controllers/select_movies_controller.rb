e5ffb0b057f4a212d30e07ced1dc3e8620173df2
Stefano Cattoni <stefanocattoni98@gmail.com>
2017-06-18 19:12:24 +0200

    Compar pelicules

--- app/controllers/select_movies_controller.rb
+++ app/controllers/select_movies_controller.rb
@@ -1,5 +1,14 @@
 class SelectMoviesController < ApplicationController
 	before_action :authenticate_user!
   def index
+  	  if params[:search]
+         @peliculas = Pelicula.search(params[:search])
+      elsif params[:search_year]
+      	 @peliculas = Pelicula.search_year(params[:search_year])
+      elsif params[:search_genre]
+      	 @peliculas = Pelicula.search_genre(params[:search_genre]).order("created_at DESC")
+      else
+      	 @peliculas = Pelicula.all
+      end
   end
 end
