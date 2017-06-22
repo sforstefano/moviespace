f8bf0b948956960c28581607a0c5ee6355a29f11
Stefano Cattoni <stefanocattoni98@gmail.com>
2017-06-22 10:33:31 +0200

    Enviar correus desde 0-2

--- app/mailers/user_notifier_mailer.rb
+++ app/mailers/user_notifier_mailer.rb
@@ -1,5 +1,5 @@
 class UserNotifierMailer < ApplicationMailer
-	default :from => 'youthcandoit@kolau.com'
+	default :from => 'stefanocattoni98@gmail.com'
 
   # send a signup email to the user, pass in the user object that   contains the user's email address
   def send_signup_email(user)
