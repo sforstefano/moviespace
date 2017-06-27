f1932aa4e4e6c262b826796b273843d4e9c479f6
Stefano Cattoni <stefanocattoni98@gmail.com>
2017-06-27 12:19:45 +0200

    Enviar correo prueba 6

--- app/mailers/user_notifier.rb
+++ app/mailers/user_notifier.rb
@@ -1,5 +1,5 @@
 class UserNotifierMailer < ApplicationMailer
-	default :from => 'movieapp1@gmail.com'
+	default :from => 'Coordinador'
 
   # send a signup email to the user, pass in the user object that   contains the user's email address
   def send_signup_email(user)
